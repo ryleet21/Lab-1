2204a5e77fefbb87da38cdcc5cf6edb4d065fca9
ryleet21 <rtaylor01@saintmarys.edu>
2024-08-28 17:22:42 -0400

    Add files via upload

---
+++ lab 1 outputs.py
@@ -0,0 +1,111 @@
+Python 3.12.5 (tags/v3.12.5:ff3bc82, Aug  6 2024, 20:45:27) [MSC v.1940 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+"Hello World!"
+'Hello World!'
+"Hello Rylee!"
+'Hello Rylee!'
+2+2
+4
+3.14 +1.1
+4.24
+3.14 - 0.14
+3.0
+print("Hello")
+Hello
+
+======================================== RESTART: C:/Users/Rylee/AppData/Local/Programs/Python/Python312/Lab 1.py =======================================
+Welcome to CPSC 207
+#the authors' name are Rylee and Serenity
+print("Hello, world")
+Hello, world
+print("Welcome to CSPSC 207")
+Welcome to CSPSC 207
+print("Hello\nWorld")
+Hello
+World
+print("How\nare\nyou")
+How
+are
+you
+print("Today", "is", "Wednesday")
+Today is Wednesday
+print("Today" + "is" + "Wednesday")
+TodayisWednesday
+
+=== RESTART: C:/Users/Rylee/AppData/Local/Programs/Python/Python312/Lab 1.py ===
+Welcome to CPSC 207
+Hello, world
+Welcome to CPSC 207
+Hello
+World
+How
+are
+you
+Today is Wednesday
+TodayisWednesday
+
+=== RESTART: C:/Users/Rylee/AppData/Local/Programs/Python/Python312/Lab 1.py ===
+Hello, world
+Welcome to CPSC 207
+Hello
+World
+How
+are
+you
+Today is Wednesday
+TodayisWednesday
+123
+45
+6
+7
+
+======================================== RESTART: C:/Users/Rylee/AppData/Local/Programs/Python/Python312/Lab 1.py =======================================
+Hello, world
+Welcome to CPSC 207
+Hello
+World
+How
+are
+you
+Today is Wednesday
+TodayisWednesday
+123
+45
+6
+7
+3
+2.3333333333333335
+2
+1
+5.5
+5
+2
+9
+8
+>>> 
+=== RESTART: C:/Users/Rylee/AppData/Local/Programs/Python/Python312/Lab 1.py ===
+Hello, world
+Welcome to CPSC 207
+Hello
+World
+How
+are
+you
+Today is Wednesday
+TodayisWednesday
+123
+45
+6
+7
+3
+2.3333333333333335
+2
+1
+5.5
+5
+2
+9
+8
+163
+2+2= 4
+50 pounds is 22.6796 Kilograms
